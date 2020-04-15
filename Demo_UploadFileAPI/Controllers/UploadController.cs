914d3093b5c039555aa2d750bb9285bb264edb0b
康文龍 <freeman6@gmail.com>
2020-04-15 12:52:42 +0800

    加入上傳檔案用的 WebAPI。

---
+++ Demo_UploadFileAPI/Controllers/UploadController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace Demo_UploadFileAPI.Controllers
+{
+    public class UploadController : ApiController
+    {
+        // POST api/values
+        public void Post()
+        {
+            var file = HttpContext.Current.Request.Files.Count > 0 ?
+                HttpContext.Current.Request.Files[0] : null;
+
+            if (file != null && file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+
+                var path = Path.Combine(
+                    HttpContext.Current.Server.MapPath("~/uploads"),
+                    fileName
+                );
+
+                file.SaveAs(path);
+            }
+        }
+    }
+}
